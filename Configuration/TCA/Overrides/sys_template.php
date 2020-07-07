b8eb3548787d838c2e8739e36a1a6b99cd08c3da
Roohi <roohi.34@gmail.com>
2020-07-07 14:21:58 +0530

    Update sys_template.php

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -14,6 +14,6 @@ call_user_func(function()
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
         $extensionKey,
         'Configuration/TypoScript',
-        'Poptin - Smart Popups and Contact Forms'
+        'Poptin: Pop ups, Contact Forms & Exit Intent Popups'
     );
 });
